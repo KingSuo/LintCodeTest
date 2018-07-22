57ba4e82e629fa06b5be53df1dd746f38a08ce03
KingSuo <648230502@qq.com>
2018-07-22 13:52:24 +0800

    合并排序数组Solution2

--- Algorithm/mergeSortedArray.py
+++ Algorithm/mergeSortedArray.py
@@ -45,4 +45,92 @@ class Solution:
                     if A[i] < B[j]:
                         new_arr.append(A[i])
                 return new_arr + B[j:]
-                
+
+class Solution2:
+    """
+    @param A: sorted integer array A
+    @param B: sorted integer array B
+    @return: A new sorted integer array
+    """
+    def mergeSortedArray(self, A, B):
+        # write your code here
+        
+        if A[0] >= B[-1]:
+            return B + A 
+        elif A[-1] <= B[0]:
+            return A + B
+        else:
+            i = j = 0
+            new_arr = []
+            if len(A) >= len(B):
+                while i < len(B):
+                    last_j = j
+                    while B[i] >= A[j]:
+                        if j == len(A) - 1:
+                            new_arr += A[last_j:]
+                            return new_arr + B[i:]
+                        j += 1
+                    new_arr += A[last_j:j]
+                    last_i = i
+                    while B[i] < A[j]:
+                        if i == len(B) - 1:
+                            new_arr += B[last_i:]
+                            return new_arr + A[j:]
+                        i += 1
+                    new_arr += B[last_i:i]
+                return new_arr + A[j:]
+            else:
+                while i < len(A):
+                    last_j = j
+                    while A[i] >= B[j]:
+                        if j == len(B) - 1:
+                            new_arr += B[last_j:]
+                            return new_arr + A[i:]
+                        j += 1
+                    new_arr += B[last_j:j]
+                    last_i = i
+                    while A[i] < B[j]:
+                        if i == len(A) - 1:
+                            new_arr += A[last_i:]
+                            return new_arr + B[j:]
+                        i += 1
+                    new_arr += A[last_i:i]
+                return new_arr + B[j:]
+
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+                                
